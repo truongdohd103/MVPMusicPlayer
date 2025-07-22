9ae223b424300b8c42cfc424100cfb8683274145
truongdohd103 <dothanhtruong2510@gmail.com>
2025-07-22 21:49:31 +0700

    Update gradle

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,4 +1,6 @@
 import org.gradle.kotlin.dsl.`kotlin-dsl`
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.api.tasks.compile.JavaCompile
 
 plugins {
     `kotlin-dsl`
@@ -6,4 +8,17 @@ plugins {
 
 repositories {
     mavenCentral()
+}
+
+// Ép jvmTarget về 17 cho tất cả các task KotlinCompile
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+// Ép sourceCompatibility và targetCompatibility cho JavaCompile về 17
+tasks.withType<JavaCompile> {
+    sourceCompatibility = "17"
+    targetCompatibility = "17"
 }
